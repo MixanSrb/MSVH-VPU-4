7b80de3ff689b2478b4e0977aa9aaf0312b677bb
MixanSrb <jmihajlo981@gmail.com>
2026-04-17 01:45:57 +0200

    Update msvh-vpu-4.cpp

--- msvh-vpu-4.cpp
+++ msvh-vpu-4.cpp
@@ -2,7 +2,9 @@
 #include <cstdint>
 #include <sstream>
 #include <string>
+#include <vector>
 using namespace std;
+
 using u4 = uint8_t;
 
 // 4-bit mask
@@ -10,10 +12,14 @@ u4 mask4(u4 x) {
     return x & 0x0F;
 }
 
-// Registers A and B
+// Registers and flags
 struct VPU {
+    // Registers
     u4 A = 0;
     u4 B = 0;
+    // Flags
+    u4 Z = 0;
+    u4 C = 0;
 
     void setA(u4 value) {
         A = mask4(value);
@@ -29,20 +35,29 @@ int main()
 {
     VPU vpu;
 
+    vector<string> program;
     string line;
 
+    cout << "Enter program (type RUN to execute):\n";
+
+    // Program input phase
     while (true)
     {
-        cout << "> ";
         getline(cin, line);
+        if (line == "RUN") break;
+        program.push_back(line);
+    }
 
-        stringstream ss(line);
+    // Execution phase
+    int PC = 0;
+
+    while (PC < program.size())
+    {
+        stringstream ss(program[PC]);
 
         string cmd;
         ss >> cmd;
 
-        // Commands
-        if (cmd.empty()) continue;
         if (cmd == "EXIT") break;
 
         if (cmd == "MOV")
@@ -50,67 +65,105 @@ int main()
             char registry;
             int value;
 
-            if (!(ss >> registry >> value))
+            if (ss >> registry >> value)
             {
-                cout << "Invalid MOV syntax. Use: MOV A 5" << endl;
-            }
-            else if (registry == 'A')
-            {
-                vpu.setA(value);
-            }
-            else if (registry == 'B')
-            {
-                vpu.setB(value);
-            }
-            else
-            {
-                cout << "Invalid registry, try A or B." << endl;
+                if (registry == 'A') vpu.setA(value);
+                else if (registry == 'B') vpu.setB(value);
             }
         }
         else if (cmd == "GET")
         {
             char registry;
             ss >> registry;
-            if (registry == 'A')
-            {
-                cout << (int)vpu.A << endl;
-            }
-            else if (registry == 'B')
-            {
-                cout << (int)vpu.B << endl;
-            }
-            else
-            {
-                cout << "Invalid registry, try A or B." << endl;
-            }
+
+            if (registry == 'A') cout << (int)vpu.A << endl;
+            else if (registry == 'B') cout << (int)vpu.B << endl;
         }
         else if (cmd == "ADD")
         {
-            vpu.setA(mask4(vpu.A + vpu.B));
-            cout << (int)vpu.A << endl;
+            int result = vpu.A + vpu.B;
+
+            vpu.C = (result > 0x0F);
+            vpu.setA(result);
+            vpu.Z = (vpu.A == 0);
         }
         else if (cmd == "SUB")
         {
-            vpu.setA(mask4(vpu.A - vpu.B));
-            cout << (int)vpu.A << endl;
+            int result = vpu.A - vpu.B;
+
+            vpu.C = (result < 0);
+            vpu.setA(result);
+            vpu.Z = (vpu.A == 0);
         }
         else if (cmd == "MUL")
         {
-            vpu.setA(mask4(vpu.A * vpu.B));
-            cout << (int)vpu.A << endl;
+            int result = vpu.A * vpu.B;
+
+            vpu.C = (result > 0x0F);
+            vpu.setA(result);
+            vpu.Z = (vpu.A == 0);
         }
         else if (cmd == "DIV")
         {
             if (vpu.B == 0)
             {
-                cout << "Divison by zero." << endl;
+                cout << "Division by zero" << endl;
             }
             else
             {
-                vpu.setA(mask4(vpu.A / vpu.B));
-                cout << (int)vpu.A << endl;
+                int result = vpu.A / vpu.B;
+
+                vpu.C = 0;
+                vpu.setA(result);
+                vpu.Z = (vpu.A == 0);
             }
         }
+        else if (cmd == "CMP")
+        {
+            vpu.Z = (vpu.A == vpu.B);
+            vpu.C = (vpu.A < vpu.B);
+
+            cout << "Z=" << (int)vpu.Z << " C=" << (int)vpu.C << endl;
+        }
+        else if (cmd == "JMP")
+        {
+            int target;
+            ss >> target;
+
+            if (target >= 0 && target < program.size())
+            {
+                PC = target;
+                continue;
+            }
+            else
+            {
+                cout << "Invalid jump address" << endl;
+            }
+        }
+        else if (cmd == "JZ")
+        {
+            int target;
+            ss >> target;
+
+            if (vpu.Z == 1)
+            {
+                PC = target;
+                continue;
+            }
+        }
+        else if (cmd == "JNZ")
+        {
+            int target;
+            ss >> target;
+
+            if (vpu.Z == 0)
+            {
+                PC = target;
+                continue;
+            }
+        }
+
+        PC++;
     }
 
     return 0;
